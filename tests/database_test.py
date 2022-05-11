20ea7783b31793cd47d8ec97e45be0c96a729946
Kartha97 <karthaveera.sgs@gmail.com>
2022-05-10 21:33:06 -0400

    test for db storing.

--- tests/database_test.py
+++ tests/database_test.py
@@ -27,3 +27,27 @@ def test_create_database_upload_folder():
     dbdir = os.path.join(root, '../app/uploads')
     # make a directory if it doesn't exist
     assert os.path.exists(dbdir) == True
+
+def test_db_storing(application):
+    with application.app_context():
+        # checking there are no users
+        assert db.session.query(User).count() == 0
+        # showing how to add a record
+        # create a record
+        user = User('test1@gmail.com', 'testtest')
+        # add it to get ready to be committed
+        db.session.add(user)
+
+        user = User.query.filter_by(email='test1@gmail.com').first()
+
+        # asserting that the user retrieved is correct which tells that it is being stored
+        assert user.email == 'test1@gmail.com'
+        db.session.commit()
+
+        # Validating there is one value in user db
+        assert db.session.query(User).count() == 1
+        # deleting user
+
+        db.session.delete(user)
+        # Validating that user is deleted or not
+        assert db.session.query(User).count() == 0
