514cad61fb556b797ffc8e418e44484412d7aa42
Kartha97 <karthaveera.sgs@gmail.com>
2022-05-10 21:24:13 -0400

    Test cases to check for csv upload link check and csv upload

--- tests/csv_upload_test.py
+++ tests/csv_upload_test.py
@@ -7,8 +7,25 @@ from app.db.models import User
 from app import db
 
 
-def test_csv(application, client):
+def test_csv_upload_link(application, client):
     res = client.get("/transactions/upload")
     print(res.data)
     assert res.status_code == 302
+
+def test_csv_upload(application, client):
+    res = client.get("/transactions/upload")
+    print(res.data)
+    assert res.status_code == 302
+
+    with application.app_context():
+        user = User('test@test.com', 'test1234')
+        filepath = 'tests/csvtest.csv'
+        testvar = ''
+        with open(filepath, encoding='utf-8-sig') as file:
+            csv_file = csv.DictReader(file)
+            for row in csv_file:
+                testvar = row
+            assert testvar == {'AMOUNT': '1000', 'TYPE': 'CREDIT'}
+        upload_res = client.post("/transactions/upload", data=testvar, follow_redirects=True)
+        assert upload_res.status_code == 200
 
