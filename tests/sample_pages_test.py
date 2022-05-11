83ae1ad437ebb23f6442e7af82b3ae3529bedf6f
Kartha97 <karthaveera.sgs@gmail.com>
2022-05-10 21:37:03 -0400

    tests for pages in my application 2.

--- tests/sample_pages_test.py
+++ tests/sample_pages_test.py
@@ -7,3 +7,10 @@ def test_request_main_menu_links(client):
     assert b'href="/about"' in response.data
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
+
+def test_request_index(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Welcome" in response.data
+
